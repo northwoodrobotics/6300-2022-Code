c4f881c0c504ad77892293afec0d99680d377af1
Kirk <robotics@northwoodschool.com>
2022-01-20 18:05:16 -0500

    init commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,85 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    public static final class DriveConstants{
+        
+        public static final double TRACKWIDTH_METERS = 1.0; // FIXME Measure and set trackwidth
+        /**
+         * The front-to-back distance between the drivetrain wheels.
+         *
+         * Should be measured from center to center.
+         */
+        public static final double WHEELBASE_METERS = 1.0; // FIXME Measure and set wheelbase
+
+        public static final SwerveDriveKinematics KINEMATICS = new SwerveDriveKinematics(
+                new Translation2d(TRACKWIDTH_METERS / 2.0, WHEELBASE_METERS / 2.0),
+                new Translation2d(TRACKWIDTH_METERS / 2.0, -WHEELBASE_METERS / 2.0),
+                new Translation2d(-TRACKWIDTH_METERS / 2.0, WHEELBASE_METERS / 2.0),
+                new Translation2d(-TRACKWIDTH_METERS / 2.0, -WHEELBASE_METERS / 2.0)
+        );
+
+        public static final double WHEEL_DIAMETER_METERS = 0.10033; // .10033 = ~4 inches
+        public static final double WHEEL_CIRCUMFERENCE_METERS = WHEEL_DIAMETER_METERS * Math.PI;
+
+        public static final int PIGEON_ID = 0; // FIXME Set Pigeon ID
+
+        public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 0; // FIXME Set front left module drive motor ID
+        public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 1; // FIXME Set front left module steer motor ID
+        public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 0; // FIXME Set front left steer encoder ID
+        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set front left steer offset
+
+        public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2; // FIXME Set front right drive motor ID
+        public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 3; // FIXME Set front right steer motor ID
+        public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 1; // FIXME Set front right steer encoder ID
+        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set front right steer offset
+
+        public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4; // FIXME Set back left drive motor ID
+        public static final int BACK_LEFT_MODULE_STEER_MOTOR = 5; // FIXME Set back left steer motor ID
+        public static final int BACK_LEFT_MODULE_STEER_ENCODER = 2; // FIXME Set back left steer encoder ID
+        public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back left steer offset
+
+        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6; // FIXME Set back right drive motor ID
+        public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7; // FIXME Set back right steer motor ID
+        public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 3; // FIXME Set back right steer encoder ID
+        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back right steer offset        
+
+        // Drivetrain Performance Mechanical limits
+        static public final double MAX_FWD_REV_SPEED_MPS = Units.feetToMeters(12.0);
+        static public final double MAX_STRAFE_SPEED_MPS = Units.feetToMeters(12.0);
+        static public final double MAX_ROTATE_SPEED_RAD_PER_SEC = Units.degreesToRadians(360.0);
+        static public final double MAX_TRANSLATE_ACCEL_MPS2 = MAX_FWD_REV_SPEED_MPS/0.25; //0-full time of 0.25 second
+        static public final double MAX_ROTATE_ACCEL_RAD_PER_SEC_2 = MAX_ROTATE_SPEED_RAD_PER_SEC/0.25; //0-full time of 0.25 second
+        public static final double MAX_VOLTAGE = 12.0; // Maximum Voltage sent to the drive motors
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // SENSOR CONSTANTS
+        // Sensor-related constants - pulled from datasheets for the sensors and gearboxes
+        static public final int ENC_PULSE_PER_REV = 2048; // TalonFX integrated sensor
+        static public final int WHEEL_ENC_COUNTS_PER_WHEEL_REV = ENC_PULSE_PER_REV;  //Assume 1-1 gearing for now
+        static public final int STEER_ENC_COUNTS_PER_MODULE_REV = 4096; // CANCoder
+        static public final double WHEEL_ENC_WHEEL_REVS_PER_COUNT  = 1.0/((double)(WHEEL_ENC_COUNTS_PER_WHEEL_REV));
+        static public final double steer_ENC_MODULE_REVS_PER_COUNT = 1.0/((double)(STEER_ENC_COUNTS_PER_MODULE_REV));
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,104 @@
+package frc.robot.subsystems;
+
+
+import frc.swervelib.Gyroscope;
+import frc.swervelib.GyroscopeHelper;
+import frc.swervelib.Mk4SwerveModuleHelper;
+import frc.swervelib.SwerveConstants;
+import frc.swervelib.SwerveModule;
+import frc.swervelib.SwerveSubsystem;
+import frc.swervelib.SwerveDrivetrainModel;
+import frc.wpiClasses.QuadSwerveSim;
+import frc.robot.Constants;
+import java.util.ArrayList;
+
+
+
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public class DrivetrainSubsystem {
+    
+
+    public static SwerveDrivetrainModel  createSwerveModel(){
+        ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
+        
+        ArrayList<SwerveModule> modules = new ArrayList<SwerveModule>(QuadSwerveSim.NUM_MODULES);
+
+        SwerveModule m_frontLeftModule = Mk4SwerveModuleHelper.createFalcon500(
+                // This parameter is optional, but will allow you to see the current state of the module on the dashboard.
+                tab.getLayout("Front Left Module", BuiltInLayouts.kList)
+                        .withSize(2, 4)
+                        .withPosition(0, 0),
+                // This can either be STANDARD or FAST depending on your gear configuration
+                Mk4SwerveModuleHelper.GearRatio.L2,
+                // This is the ID of the drive motor
+                Constants.DriveConstants.FRONT_LEFT_MODULE_DRIVE_MOTOR,
+                // This is the ID of the steer motor
+                Constants.DriveConstants.FRONT_LEFT_MODULE_STEER_MOTOR,
+                // This is the ID of the steer encoder
+                Constants.DriveConstants.FRONT_LEFT_MODULE_STEER_ENCODER,
+                // This is how much the steer encoder is offset from true zero (In our case, zero is facing straight forward)
+                Constants.DriveConstants.FRONT_LEFT_MODULE_STEER_OFFSET, "FL"
+        );
+    
+        // We will do the same for the other modules
+        SwerveModule m_frontRightModule = Mk4SwerveModuleHelper.createFalcon500(
+                tab.getLayout("Front Right Module", BuiltInLayouts.kList)
+                        .withSize(2, 4)
+                        .withPosition(2, 0),
+                Mk4SwerveModuleHelper.GearRatio.L2,
+                Constants.DriveConstants.FRONT_RIGHT_MODULE_DRIVE_MOTOR,
+                Constants.DriveConstants.FRONT_RIGHT_MODULE_STEER_MOTOR,
+                Constants.DriveConstants.FRONT_RIGHT_MODULE_STEER_ENCODER,
+                Constants.DriveConstants.FRONT_RIGHT_MODULE_STEER_OFFSET, "FR"
+        );
+    
+        SwerveModule m_backLeftModule = Mk4SwerveModuleHelper.createFalcon500(
+                tab.getLayout("Back Left Module", BuiltInLayouts.kList)
+                        .withSize(2, 4)
+                        .withPosition(4, 0),
+                Mk4SwerveModuleHelper.GearRatio.L2,
+                Constants.DriveConstants.BACK_LEFT_MODULE_DRIVE_MOTOR,
+                Constants.DriveConstants.BACK_LEFT_MODULE_STEER_MOTOR,
+                Constants.DriveConstants.BACK_LEFT_MODULE_STEER_ENCODER,
+                Constants.DriveConstants.BACK_LEFT_MODULE_STEER_OFFSET, "BL"
+        );
+    
+        SwerveModule m_backRightModule = Mk4SwerveModuleHelper.createFalcon500(
+                tab.getLayout("Back Right Module", BuiltInLayouts.kList)
+                        .withSize(2, 4)
+                        .withPosition(6, 0),
+                Mk4SwerveModuleHelper.GearRatio.L2,
+                Constants.DriveConstants.BACK_RIGHT_MODULE_DRIVE_MOTOR,
+                Constants.DriveConstants.BACK_RIGHT_MODULE_STEER_MOTOR,
+                Constants.DriveConstants.BACK_RIGHT_MODULE_STEER_ENCODER,
+                Constants.DriveConstants.BACK_RIGHT_MODULE_STEER_OFFSET, "BR"
+        );
+
+        Gyroscope gyro = GyroscopeHelper.createnavXMXP();
+
+        modules.add(m_frontLeftModule);
+        modules.add(m_frontRightModule);
+        modules.add(m_backLeftModule);
+        modules.add(m_backRightModule);
+        
+
+
+        return new SwerveDrivetrainModel(modules, gyro);
+
+
+    }
+    public static SwerveSubsystem createSwerveSubsystem(SwerveDrivetrainModel dt) {
+        return new SwerveSubsystem(dt);        
+    }
+
+    
+    
+}
