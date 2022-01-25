d05c95f852a5acb6a436d2a26cc7e983618438fb
northwoodrobotics <robotics@northwoodschool.com>
2022-01-25 17:21:10 -0500

    some changes

--- src/main/java/frc/robot/commands/DriveCommands/AutoDrive.java
+++ src/main/java/frc/robot/commands/DriveCommands/AutoDrive.java
@@ -1,4 +1,4 @@
-package frc.robot.commands;
+package frc.robot.commands.DriveCommands;
 
 
 import com.pathplanner.lib.PathPlannerTrajectory;
--- src/main/java/frc/robot/commands/DriveCommands/TeleopDriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommands/TeleopDriveCommand.java
@@ -1,4 +1,4 @@
-package frc.robot.commands;
+package frc.robot.commands.DriveCommands;
 
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
