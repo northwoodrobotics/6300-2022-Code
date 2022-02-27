568eb6bb06cbc244b0d9c781306affde53f4ee00
Kirk <robotics@northwoodschool.com>
2022-02-27 12:43:42 -0500

    add climber

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -144,12 +144,21 @@ public final class Constants {
                 kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
     }
     public final static class ClimberConstants{
-        public static final double RevEncoder_CountsPer_Rev = 8192;
+        public static final int RevEncoder_CountsPer_Rev = 8192;
         public static final int ClimbMotor1 = 27;
+        public static final int ClimbMotor2= 26;
+        public static final int ClimbSolenoid = 2;
+        public static final int BalanceSolenoid =3;
+        public static final int CatchSolenoid = 4;
         
         public static final double SpoolDiameter = Units.inchesToMeters(1);
 
         public static final double MidRungSetpoint = Units.inchesToMeters(0);
+
+        public static final double Climb1P = 0.0;
+        public static final double Climb1D = 0.0;
+        public static final double Climb1I = 0.0;
+        
 
 
 
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,14 +1,59 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.google.errorprone.annotations.concurrent.GuardedBy;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.EncoderType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.ClimberConstants;
 
 public class ClimberSubsystem extends SubsystemBase{
-    private TalonSRX TestClimbMotor = new TalonSRX(31);
-    /*real stuff
-    private CANSparkMax ClimbMotor1 = new CANSparkMax()
-    */
+    //private TalonSRX TestClimbMotor = new TalonSRX(31);
+    //real stuff
+    private CANSparkMax ClimbMotor1 = new CANSparkMax(Constants.ClimberConstants.ClimbMotor1, MotorType.kBrushless);
+    private CANSparkMax ClimbMotor2 = new CANSparkMax(Constants.ClimberConstants.ClimbMotor2, MotorType.kBrushless);
+    private Solenoid lockSolenoid = new Solenoid(PneumaticsModuleType.REVPH, ClimberConstants.ClimbSolenoid);
+    private Solenoid BalanceSolenoid = new Solenoid(PneumaticsModuleType.REVPH, ClimberConstants.BalanceSolenoid);
+    private Solenoid CatchSolenoid = new Solenoid(PneumaticsModuleType.REVPH, ClimberConstants.CatchSolenoid);
+    private SparkMaxPIDController Climb1Controller;
+    private SparkMaxPIDController Climb2Controller;
+    private RelativeEncoder Climb1Encoder;
+    private RelativeEncoder Climb2Encoder;
+    private final Object stateLock = new Object();
+
+    @GuardedBy("stateLock")
+    private boolean LatchOn = false;
+    @GuardedBy("stateLock")
+    private boolean ClimbUp = false;
+    
+
+    public ClimberSubsystem(){
+        setBreakModes();
+        Climb1Controller = ClimbMotor1.getPIDController();
+        Climb2Controller = ClimbMotor2.getPIDController();
+        Climb1Encoder = ClimbMotor1.getAlternateEncoder(ClimberConstants.RevEncoder_CountsPer_Rev);
+        Climb2Encoder = ClimbMotor2.getAlternateEncoder(ClimberConstants.RevEncoder_CountsPer_Rev);
+
+
+        
+    }
+    
+    public void setBreakModes(){
+        ClimbMotor1.setIdleMode(IdleMode.kBrake);
+        ClimbMotor2.setIdleMode(IdleMode.kBrake);
+    }
+
+
+
     
     
 }
