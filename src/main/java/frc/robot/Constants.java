4f3f6bf6ba92207eceb87109650ef28761aba848
kirk <minhkhoi.nguyenle@gmail.com>
2022-11-02 10:21:08 -0700

    change canID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,8 +72,8 @@ public final class Constants {
         public static final int BACK_LEFT_MODULE_STEER_MOTOR = 10; // FIXME Set back left steer motor ID
         public static final int BACK_LEFT_MODULE_STEER_ENCODER = 12; // FIXME Set back left steer encoder ID
         public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(261.035); // FIXME Measure and set back left steer offset
-        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 15; // FIXME Set back right drive motor ID
-        public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 14; // FIXME Set back right steer motor ID
+        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 14; // FIXME Set back right drive motor ID
+        public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 15; // FIXME Set back right steer motor ID
         public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 16; // FIXME Set back right steer encoder ID
         public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(208.65); // FIXME Measure and set back right steer offset        
         // Drivetrain Performance Mechanical limits
